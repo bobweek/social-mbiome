ad9f5973d693cd5a0439b63f36057d857951a9f0
bobweek <bweek.ecoevo@proton.me>
2025-02-06 15:46:33 +0100

    v0.1

--- sde-fcts.jl
+++ sde-fcts.jl
@@ -1,41 +1,210 @@
-using Parameters, StochasticDiffEq, DataFrames, CSV, UnicodePlots
+using Parameters, StochasticDiffEq, LinearAlgebra, CSV, DataFrames, Plots, PlotThemes
+
+"""
+# model parameters
+
+- T = maximum time to run simulation to
+- K = number of hosts
+- S = number of microbial taxa
+- Ψ = social transmission network
+- Ξ = environmental transmission network
+- r = intrinsic growth rates
+- α = competition coefficients
+- s = social transmission
+- e = environmental transmission
+- β = magnitudes of demographic stochasticity
+- N₀ = initial abundances
+
+"""
+@with_kw mutable struct PARS
+
+    # SYNTAX
+
+    # the `::` assigns a type to the variable
+    # the `=` assigns a default value
+
+    # so `T::Float64 = 10.0` defines a 
+    # continuous value `T` taking `10.0` as a default
+
+    # maximum time to run simulation to
+    T::Float64 = 10.0
+
+    # number of hosts
+    K::Int64 = 4
+
+    # number of microbial taxa
+    S::Int64 = 2
+
+    # social transmission network
+    Ψ::Matrix{Float64} = Symmetric(abs.(randn(K,K))).*(ones(K,K)-I)
+
+    # environmental transmission network
+    Ξ::Matrix{Float64} = Symmetric(abs.(randn(K,K))).*(ones(K,K)-I)
+    
+    # intrinsic growth rates
+    # r[i,k] is growth rate of microbe i in host k
+    r::Matrix{Float64} = 10*ones(S,K)
+
+    # competition coefficients
+    # α[k][i,j] is coeff between microbes i and j in host k
+    α::Vector{Matrix{Float64}} = 0.1*fill(abs.(randn(S,S)),K)
+
+    # social transmission
+    s::Vector{Float64} = 0.001*ones(S)
+
+    # environmental transmission
+    e::Vector{Float64} = 0.001*ones(S)
+
+    # magnitudes of demographic stochasticity
+    β::Matrix{Float64} = ones(S,K)
+
+    # initial abundances
+    N₀::Matrix{Float64} = 100*ones(S,K)
+
+end
 
 # deterministic component
-function f!(du, u, p, t)
+function f(N, p, t)
 
-    # remove negative abundances (due to numerical round-off errors)
-    ninds = findall(x->x<0,u)
-    u[ninds] .= 0.0
+    # # remove negative abundances (due to numerical round-off errors)
+    ninds = findall(x->x<0,N)
+    N[ninds] .= 0.0
 
-    K = floor(Int64,p[1])
-    r = p[2:(K+1)]
-    α = p[(K+2):(2*K+1)]
-    s = p[(2*K+2):(3*K+1)]
-    Ψ = reshape(last(p,K*K),K,K)
+    # unpack parameters
+    @unpack_PARS p
 
-    # deterministic differentials
-    d = (r - α .* u) .* u  +  s .* Ψ * u
-
+    # turns the competition bit into a matrix
+    αN::Matrix{Float64} = zeros(S,K)
     for k in 1:K
-        du[k] = d[k]
+        αN[:,k] = α[k]*N[:,k] # the * here does matrix multiplication
     end
+
+    # turns transmission bits into matrices
+    sN::Matrix{Float64} = zeros(S,K)
+    eN::Matrix{Float64} = zeros(S,K)
+    for k in 1:K
+        sN[:,k] = s .* N[:,k] # the .* does element-wise multiplication
+        eN[:,k] = e .* N[:,k] # the .* does element-wise multiplication
+    end
+
+    # effects of within-host dynamics
+    WTHN_HOST = (r - αN) .* N # the .* does element-wise multiplication
+
+    # effects of between-host dynamics (ie, transmission)
+    BTWN_HOST = sN*Ψ + sN*Ξ
+
+    # net deterministic dynamics
+    dN = WTHN_HOST + BTWN_HOST
+
+    return dN
 
 end
 
 # stochastic component
-function g!(du, u, p, t)
+function g(N, p, t)
 
     # remove negative abundances (due to numerical round-off errors)
-    ninds = findall(x->x<0,u)
-    u[ninds] .= 0.0
+    ninds = findall(x->x<0,N)
+    N[ninds] .= 0.0
 
-    K = floor(Int64,p[1])
-    β = p[(3*K+2):(4*K+1)]
+    # unpack model parameter
+    @unpack_PARS p
+ 
+    dN = β .* .√N
 
-    d = β .* .√u
-
-    for k in 1:K
-        du[k] = d[k]
-    end
+    return dN
 
 end
+
+function runSim(p::PARS)
+    @unpack_PARS p
+
+    # define timespan to simulate over
+    tspan = (0.0,T)
+
+    # run simulation
+    prob = SDEProblem(f, g, N₀, tspan, p)
+    out = solve(prob, SRIW1())
+
+    return out
+
+end
+
+# saves parameter values as csv with filename fn
+function savePARS(p::PARS;rbN0="rbN0",alpha="alpha",se="se",TKS="TKS",social="social",enviro="enviro")
+    @unpack_PARS p
+
+    # each column is a vectorized matrix of parameters
+    # column lengths are S*K
+    dfSK = DataFrame(r = vec(r), β = vec(β), N₀ = vec(N₀))
+
+    # each column is a vectorized matrix of comp coeffs for host k
+    # column lengths are S*S
+    dfSS = DataFrame(α1 = vec(α[1]))
+    for k in 2:K
+        nm = "α"*string(k)
+        dfSS[!,nm] = vec(α[k])
+    end
+
+    # each column is vector of microbe transmission "propensities"
+    # so each column is length S
+    dfS = DataFrame(s=s, e=e)
+
+    # simulation time T, number of hosts K, and number of microbes s
+    # ... each column length 1
+    df1 = DataFrame(T=T,K=K,S=S)
+
+    # social transmission matrix
+    dfΨ = DataFrame(PSIx1 = Ψ[:,1])
+    for k in 2:K
+        nm = "PSIx"*string(k)
+        dfΨ[!,nm] = Ψ[:,k]
+    end
+
+    # environmental transmission matrix
+    dfΞ = DataFrame(XIx1 = Ξ[:,1])
+    for k in 2:K
+        nm = "XIx"*string(k)
+        dfΞ[!,nm] = Ξ[:,k]
+    end
+
+    CSV.write(rbN0*".csv", dfSK)
+    CSV.write(alpha*".csv", dfSS)
+    CSV.write(se*".csv", dfS)
+    CSV.write(TKS*".csv", df1)
+    CSV.write(social*".csv", dfΨ)
+    CSV.write(enviro*".csv", dfΞ)
+
+
+end
+
+# saves time series as csv with filename fn
+function saveTS(out;abunds="abunds",times="times",pts=200)
+
+    n = length(out.t)
+
+    if pts > n
+        println("\nMore time points requested than available
+                 \nDefaulting to maximum time points available: pts=",n)
+        pts = n
+    end
+
+    inds = round.(Int,LinRange(1,n,pts))
+
+    # time points
+    # single column of length pts
+    dft = DataFrame(t=out.t[inds])
+
+    # microbe abundances
+    # each column is the S*K abundances at a time pt
+    dfN = DataFrame(N_t1 = vec(out.u[1]))
+    for k in 2:pts
+        nm = "N_t"*string(k)
+        dfN[!,nm] = vec(out.u[k])
+    end
+    
+    CSV.write(times*".csv", dft)
+    CSV.write(abunds*".csv", dfN)
+    
+end
+    
--- simple-sim.jl
+++ simple-sim.jl
@@ -1,35 +1,141 @@
 include("sde-fcts.jl")
 
-# model parameters
-m = CSV.read("model-parameters.csv",DataFrame)
+#############################
+#
+# SETTING UP MODEL PARAMETERS
+#
+#############################
 
-# can read binary or continuous valued edge matrix
-Ψ = CSV.File("network.csv", header=false) |> Tables.matrix
 
-# simulation parameters
-s = CSV.read("sim-parameters.csv",DataFrame)
+# this contains the model parameters
+#   and running it "empty" assigns
+#   default parameter values
+# hovering over PARS() should give a summary
+p = PARS()
 
-# p is the "vector" of parameters (where each entry can be any kind of object)
-p = Vector{Float64}(undef,1)
-p[1] = length(m.r) # = K, the number of hosts
-append!(p,m.r)
-append!(p,m.α)
-append!(p,m.s)
-append!(p,m.β)
-append!(p,vec(Ψ))
+# you can change parameter values as follows
+p.K = 20;
+p.S = 10;
+p.r = zeros(p.S,p.K);
 
-tspan = (0.0, s.T[1])
-
-# numerically solve SDE
-prob = SDEProblem(f!, g!, m.N₀, tspan, p);
-sol = solve(prob, SRIW1(), progress=true)
-
-N = reduce(hcat,sol.u)'
-
-pl = lineplot(sol.t,N,xlabel="time",ylabel="N",ylim=(0,maximum(N)),name=["N1","N2","N3","N4"])
-
-print(pl)
+# or you can set them while calling the constructor
+p = PARS(K=20,S=10);
 
 #
-# for reference see: https://docs.sciml.ai/DiffEqDocs/stable/tutorials/sde_example/
+# TODO: define p by loading in a csv file ...
 #
+
+
+
+
+#######################
+#
+# RUN THE SIMULATION !
+#
+#######################
+
+
+# the simulation is ran simply as follows``
+out = runSim(p)
+
+# NOTE:
+#   if it says "DtLessThanMin"
+#   then you probably made some competition coeffs negative
+#   and a mutualistic orgy is taking place
+#   julia can't handle infinite abundances ... so it aborts
+
+
+
+
+#######################
+#
+# PLOTTING THE OUTPUT
+#
+#######################
+
+
+theme(:orange) # i just love this theme
+
+# you can plot the dynamics of all microbe abundances simultaneously with
+plot(out,legend=false,background_color=:transparent,background_color_inside=:transparent)
+
+
+
+
+
+#######################
+#
+# SAVING THE PARAMETERS
+#
+#######################
+
+
+# this saves the model parameters to a csv file
+#       using default file names
+savePARS(p)
+
+# you can specify filenames like
+savePARS(p,rbN0="rbN0_i")
+
+# or like
+savePARS(p,TKS="SOMETHING_COMPLETELY_DIFFERENT",alpha="AlpHa",enviro="🐛🍔🌞")
+
+
+########################
+#
+# SAVING THE TIME SERIES
+#
+########################
+
+
+# this writes the time series data to a csv file named "abunds.csv"
+#   also provides a csv named "times.csv" with time points as a column
+saveTS(out)
+
+# filenames can be changed as
+saveTS(out,abunds="N",times="t")
+
+# temporal resolution can also be changed like
+saveTS(out,pts=10)
+
+# trying for more time points than whats available
+#   just defaults to max time points available
+saveTS(out,pts=1e100)
+
+
+###############################
+#
+# A NOTE ON MATRICIZING VECTORS
+#
+###############################
+
+# data are written to csv's in "vectorized" form
+# that means if something is a matrix (like r or α[k])
+# then it is first made into vector where its columns are
+# stacked on top of each other, and then this vector is 
+# made a column in the associated csv file
+
+# so you might want to take one of these vectorized matrices
+# while in R or python or ... and turn it back into a matrix
+
+# below is some code for doing this in julia, where the original
+# matrix is assumed to have S rows and K columns... hope this is helpful!
+
+
+# matricize a vector that was once a S×K matrix
+function mat(v,S,K)
+
+    # check if this makes sense
+    if length(v) != S*K
+        println("DIMENSION MISMATCH ☹️")
+    end
+
+    # do the thing
+    m = zeros(S,K)
+    for k in 1:K
+        m[:,k] = v[1+(k-1)*S:k*S]
+    end
+
+    return m
+
+end
